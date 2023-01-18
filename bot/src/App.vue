176d23fac671d8ccbbf0e02e3d6060af63331632
Matías García Gimeno (mgg709) <mgg709@inlumine.ual.es>
2023-01-18 20:29:22 +0100

    subida inicial

---
+++ bot/src/App.vue
@@ -0,0 +1,21 @@
+<script></script>
+
+<template>
+  <div :style="container">
+    <button>Seguimiento</button>
+    <button>Bot</button>
+    <button>Consejo</button>
+  </div>
+</template>
+
+<style>
+.container {
+  height: 100vh;
+  width: 100vh;
+}
+img {
+  width: 100vh;
+  height: 100vh;
+  background-size: cover;
+}
+</style>
