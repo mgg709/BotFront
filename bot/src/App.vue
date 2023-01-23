5f2184129387ec5e03ffd9b0983c35f21479af9f
Matías García Gimeno (mgg709) <mgg709@inlumine.ual.es>
2023-01-23 20:31:46 +0100

    modificacion pantalla inicial

--- bot/src/App.vue
+++ bot/src/App.vue
@@ -3,15 +3,15 @@
 <template>
   <div class="content">
     <span>
-      <button>Seguimiento</button>
-      <button>Bot</button>
+      <button class="img-btn">Seguimiento</button>
+      <button class="img-btn"><img /></button>
       <button>Consejo</button>
     </span>
   </div>
 </template>
 
 <style>
-button {
+/* button {
   padding: 10px;
   background: none;
   border: 1px solid white;
@@ -25,11 +25,32 @@ button {
 button:hover {
   background: white;
   color: #333;
-}
+} */
 
 .content {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+button.img-btn {
+  text-decoration: none;
+  padding: 0;
+  border-radius: 10px;
+  background: linear-gradient(#2940d3, #001abd 64%);
+  width: 100px;
+  height: 100px;
+  cursor: pointer;
+  margin: 4em;
+  border: none;
+  box-shadow: 0px 0px 5px black;
+}
+
+button.img-btn img {
+  content: url("src/assets/robot.png");
+}
+
+button.img-btn:hover img {
+  content: url("src/assets/robot-hover.png");
 }
 </style>
