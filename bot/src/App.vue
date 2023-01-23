c5a85a9ac91b67a5287de1c7059fba4dc907dd47
Matías García Gimeno (mgg709) <mgg709@inlumine.ual.es>
2023-01-23 20:52:31 +0100

    animaciones botones

--- bot/src/App.vue
+++ bot/src/App.vue
@@ -4,29 +4,13 @@
   <div class="content">
     <span>
       <button class="img-btn">Seguimiento</button>
-      <button class="img-btn"><img /></button>
-      <button>Consejo</button>
+      <button class="img-btn"><img class="bot" />Bot</button>
+      <button class="img-btn">Consejo</button>
     </span>
   </div>
 </template>
 
 <style>
-/* button {
-  padding: 10px;
-  background: none;
-  border: 1px solid white;
-  color: white;
-  font-size: 20px;
-  cursor: pointer;
-  transition: all 0.2s ease-in-out;
-  margin: 5px;
-}
-
-button:hover {
-  background: white;
-  color: #333;
-} */
-
 .content {
   display: flex;
   flex-direction: column;
@@ -44,13 +28,49 @@ button.img-btn {
   margin: 4em;
   border: none;
   box-shadow: 0px 0px 5px black;
+  transition: 0.5s, color 0.1s;
+  -webkit-transition: 0.5s, color 0.1s;
+  -moz-transition: 0.5s, color 0.1s;
+}
+button.img-btn:hover {
+  text-decoration: none;
+  padding: 0;
+  border-radius: 10px;
+  background: linear-gradient(#ffffff, #a2a2a2);
+  width: 100px;
+  height: 100px;
+  cursor: pointer;
+  margin: 4em;
+  border: none;
+  box-shadow: 0px 0px 5px black;
+  transform: scale3d(1.2, 1.2, 0.3);
+  -webkit-transform: scale3d(1.2, 1.2, 0.3);
+  -moz-transform: scale3d(1.2, 1.2, 0.3);
 }
 
-button.img-btn img {
+button.img-btn img.bot {
+  content: url("src/assets/robot-only.png");
+  padding: 8px;
+}
+
+button.img-btn:hover img.bot {
+  content: url("src/assets/robot-only.png");
+  padding: 8px;
+}
+
+button.img-btn img.consejo {
   content: url("src/assets/robot.png");
 }
 
-button.img-btn:hover img {
+button.img-btn:hover img.consejo {
+  content: url("src/assets/robot-hover.png");
+}
+
+button.img-btn img.seguimiento {
+  content: url("src/assets/robot.png");
+}
+
+button.img-btn:hover img.seguimiento {
   content: url("src/assets/robot-hover.png");
 }
 </style>
