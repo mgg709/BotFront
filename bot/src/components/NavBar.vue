3d33c5d7cf373f3e36e5ce9c07ed75a34df1168d
Matías García Gimeno (mgg709) <mgg709@inlumine.ual.es>
2023-01-24 19:18:17 +0100

    nav bar avanzada

--- bot/src/components/NavBar.vue
+++ bot/src/components/NavBar.vue
@@ -1,18 +1,55 @@
 <template>
-  <div class="bar">
-    <RouterLink to="/dashboard" class="element">Panel de control</RouterLink>
-    <RouterLink to="/dashboard" class="element">Bot</RouterLink>
-    <RouterLink to="/dashboard" class="element">Consejo</RouterLink>
-  </div>
+  <header>
+    <div class="bar">
+      <RouterLink to="/dashboard" class="element1">Panel de control</RouterLink>
+      <RouterLink to="/dashboard" class="element2">Bot</RouterLink>
+      <RouterLink to="/dashboard" class="element3">Consejo</RouterLink>
+    </div>
+  </header>
 </template>
 
 <style>
-.bar {
-  background: #ffffff;
-  border-bottom: 4px solid #3777e1;
+header {
+  display: flex;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
   width: 100%;
   max-width: 100%;
   max-height: auto;
-  height: 66px;
+  height: 65px;
+  border-bottom: 4px solid #3777e1;
+  box-shadow: 0 3px 10px #000;
+  background-color: #232323;
+}
+.bar {
+  display: flex;
+  margin-left: 150px;
+  margin-right: 150px;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  position: fixed;
+  width: 70%;
+  max-width: 100%;
+  max-height: auto;
+  height: 65px;
+}
+
+.element1 {
+  color: white;
+
+  text-decoration: none;
+}
+
+.element2 {
+  color: white;
+  text-decoration: none;
+}
+
+.element3 {
+  color: white;
+  text-decoration: none;
 }
 </style>
